c4eacbe8b783c116675d9ff43f16c65f13704a0b
Arup Mondal <mondalconnect17@gmail.com>
2022-11-24 11:27:42 +0530

    App

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Modal from './Modal';
+import Sidebar from './Sidebar';
+import Home from './Home';
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>
+  )
+}
+
+export default App
+// @mondalcodehub (November-2022)
