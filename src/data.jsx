ee0e5cdee93b38a9668dc8d6da80e5c005363114
Arup Mondal <mondalconnect17@gmail.com>
2022-11-24 11:25:23 +0530

    data

---
+++ src/data.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import {
+    FaGithub,
+    FaFacebook,
+    FaLinkedin,
+    FaTwitter,
+    FaInstagram,
+    FaHome,
+    FaUserFriends,
+    FaFolderOpen,
+    FaCalendarAlt,
+    FaWpforms,
+} from 'react-icons/fa';
+export const links = [
+    {
+        id: 1,
+        url: '/',
+        text: 'home',
+        icon: <FaHome />,
+    },
+    {
+        id: 2,
+        url: '/team',
+        text: 'team',
+        icon: <FaUserFriends />,
+    },
+    {
+        id: 3,
+        url: '/projects',
+        text: 'projects',
+        icon: <FaFolderOpen />,
+    },
+    {
+        id: 4,
+        url: '/calendar',
+        text: 'calendar',
+        icon: <FaCalendarAlt />,
+    },
+    {
+        id: 5,
+        url: '/documents',
+        text: 'documents',
+        icon: <FaWpforms />,
+    },
+];
+
+export const social = [
+    {
+        id: 1,
+        url: 'https://www.facebook.com',
+        icon: <FaFacebook />,
+    },
+    {
+        id: 2,
+        url: 'https://www.twitter.com',
+        icon: <FaTwitter />,
+    },
+    {
+        id: 3,
+        url: 'https://www.linkdin.com',
+        icon: <FaLinkedin />,
+    },
+    {
+        id: 4,
+        url: 'https://www.github.com/mondalCodeHub',
+        icon: <FaGithub />,
+    },
+    {
+        id: 5,
+        url: 'https://www.instagram.com',
+        icon: <FaInstagram />,
+    },
+];
+
+// @mondalcodehub (November-2022)
